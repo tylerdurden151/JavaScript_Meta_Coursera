e121ae5d26a52e6cf518cc64ade94d36b24e54b9
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-17 16:10:47 -0700

    Completed

--- 03-Multiple-components/src/App.jsx
+++ 03-Multiple-components/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import Card from './components/Card'
 import './App.css'
 
 
@@ -9,6 +10,10 @@ function App() {
   return (
     <div>
       <h1>Task: Add three Card elements</h1>
+      <Card h2="First card's h2" h3="First card's h3" />
+      <Card h2="Second card's h2" h3="Second card's h3" />
+      <Card h2="Third card's h2" h3="Third card's h3" />
+      
     </div>
   );
 }
