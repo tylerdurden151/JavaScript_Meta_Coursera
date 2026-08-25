68fee68c796b4587767370dfbde748d35c1dfee2
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-25 15:09:45 -0700

    Modified imports

--- 03-Multiple-components/src/App.jsx
+++ 03-Multiple-components/src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import Card from './components/Card'
-import './App.css'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import Card from "./components/Card";
+import "./App.css";
 
 function App() {
   return (
@@ -13,10 +12,8 @@ function App() {
       <Card h2="First card's h2" h3="First card's h3" />
       <Card h2="Second card's h2" h3="Second card's h3" />
       <Card h2="Third card's h2" h3="Third card's h3" />
-      
     </div>
   );
 }
 
-
-export default App
+export default App;
